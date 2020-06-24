bd9c40e206cf80b03da736349e52da3720e7eaa3
Dedi Irawan <38521675+dedisaurus@users.noreply.github.com>
2020-06-24 10:35:51 +0700

    tess

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/todo', todoRouter);
 app.use('/api-docs-sum', swaggerUi.serve, swaggerUi.setup(swaggerSumDocument));
 
 
-mongoose.connect('mongodb://localhost/crud')  
+mongoose.connect('mongodb://127.0.0.1/crud')  
   .then(() => console.log('Berhasil terhubung dengan MongoDB'))
   .catch((err) => console.error(err));
 

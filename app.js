b2db357bbd1679e26b2ea7fc7e4b89053e28ff34
Dedi Irawan <38521675+dedisaurus@users.noreply.github.com>
2020-06-24 10:48:11 +0700

    tes

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSumDocument = require ('./swaggerSumDocument.json');
 
-mongoose.Promise = global.Promise;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var todoRouter = require('./routes/todo');
@@ -22,8 +21,25 @@ app.use('/users', usersRouter);
 app.use('/todo', todoRouter); 
 app.use('/api-docs-sum', swaggerUi.serve, swaggerUi.setup(swaggerSumDocument));
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-mongoose.connect('mongodb://localhost:27017/crud')  
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1/crud')  
   .then(() => console.log('Berhasil terhubung dengan MongoDB'))
   .catch((err) => console.error(err));
 
